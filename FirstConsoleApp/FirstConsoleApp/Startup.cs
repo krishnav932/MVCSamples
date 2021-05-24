649bc4c490e5b18711df82c600f72a7c7a310a4c
krishnav932 <krishnav932@gmail.com>
2021-05-24 09:43:58 +0530

    samples on MVC

---
+++ FirstConsoleApp/FirstConsoleApp/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    public class Startup
+    { 
+        public void ConfigurationServices(IServiceCollection service)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/",async context =>
+                {
+                    await context.Response.WriteAsync("Hello from Cognine Application");
+                });
+            });
+        }
+    }
+}
